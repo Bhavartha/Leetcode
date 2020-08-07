af5708e397d6c3daf22e048e211b29cab7fee410
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-08-07 18:38:52 +0530

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+    vector<int> maxSlidingWindow(vector<int>& v, int k) {
         vector<int> ans;
-        multiset<int> mx;
-        for(int i=0;i<k;i++)
-            mx.insert(nums[i]);
-        ans.push_back(*(mx.rbegin()));
-        for(int i=0;i<nums.size()-k;i++){
-            mx.erase(mx.find(nums[i]));
-            mx.insert(nums[i+k]);
-            ans.push_back(*(mx.rbegin()));
+        deque<int> dq;
+        for(int i=0;i<k;i++){
+            while(!dq.empty() && v[dq.back()]<=v[i])
+                dq.pop_back();
+            dq.push_back(i);
         }
-        return ans;            
+        ans.push_back(v[dq.front()]);
+        for(int i=k;i<v.size();i++){
+            while(!dq.empty() && dq.front()<=(i-k))
+                dq.pop_front();
+            while(!dq.empty() && v[dq.back()]<=v[i])
+                dq.pop_back();
+            dq.push_back(i);
+            ans.push_back(v[dq.front()]);
+        }
+        return ans;
     }
 };

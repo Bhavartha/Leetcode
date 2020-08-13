b97193683afad5632f0ad467293058b86de9ab27
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-08-13 22:44:25 +0530

    Create Minimum Subsequence in Non-Increasing Order.cpp

---
+++ Minimum Subsequence in Non-Increasing Order.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> minSubsequence(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        int sum=accumulate(nums.begin(), nums.end(), 0);
+        vector<int>ans;
+        int tmp=0;
+        for(int n:nums){
+            ans.push_back(n);
+            tmp+=n;
+            if(tmp*2>sum)
+                break;
+        }
+        return ans;
+    }
+};

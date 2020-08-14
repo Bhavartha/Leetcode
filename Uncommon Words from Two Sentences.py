461c50faa452513b36acf3975b946d9c8ac59265
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-08-14 18:48:32 +0530

    Update Uncommon Words from Two Sentences.py

--- Uncommon Words from Two Sentences.py
+++ Uncommon Words from Two Sentences.py
@@ -2,11 +2,9 @@ from collections import Counter
 
 class Solution:
     def uncommonFromSentences(self, A: str, B: str) -> List[str]:
-        A=A.split()
-        B=B.split()
-        d = dict(Counter(A+B))
+        d = dict(Counter((A+" "+B).split()))
         ans=[]
         for x in d:
             if d[x]==1:
                 ans.append(x)
-        return ans       
+        return ans

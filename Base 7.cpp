c84111761a875588d15a7015e0811ca24764ec9a
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-08-15 16:17:16 +0530

    Create Base 7.cpp

---
+++ Base 7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if(num==0)return "0";
+        bool neg=false;
+        if(num<0){
+            neg=true;
+            num=-num;
+        }
+        string ans="";
+        while(num>0){
+            ans=to_string(num%7)+ans;
+            num/=7;
+        }
+        if(neg) ans="-"+ans;
+        return ans;
+    }
+};

c73d717341f933d6797c186efafa0917c092395b
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2020-08-15 14:50:20 +0530

    Update Flipping an Image.cpp

--- Flipping an Image.cpp
+++ Flipping an Image.cpp
@@ -4,12 +4,10 @@ public:
         int r=img.size();
         int c=img[0].size();
         
-        for(int i=0;i<r;i++)
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++) img[i][j]^=1;
             reverse(img[i].begin(),img[i].end());
-        for(int i=0;i<r;i++)
-            for(int j=0;j<c;j++)
-                img[i][j]^=1;
-        
+        }        
         return img;
     }
 };

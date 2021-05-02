71ff2488991c61233dafda65b9456bc4204d0bd8
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2021-05-02 17:46:07 +0530

    Update Next Greater Element II.cpp

--- Next Greater Element II.cpp
+++ Next Greater Element II.cpp
@@ -2,22 +2,15 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0;i<n;i++)
-            nums.push_back(nums[i]);
         stack<int> ss;
-        for(int i=0;i<nums.size();++i){
-            while(!ss.empty() && nums[ss.top()]<nums[i]){
-                nums[ss.top()]=nums[i];
+        vector<int> t(n,-1);
+        for(int i=0;i<n*2;++i){
+            while(!ss.empty() && nums[ss.top()]<nums[i%n]){
+                t[ss.top()]=nums[i%n];
                 ss.pop();
             }
-            ss.push(i);
+            ss.push(i%n);
         }
-        while(!ss.empty()){
-            nums[ss.top()]= -1;
-            ss.pop();
-        }
-        for(int i=0;i<n;i++)
-            nums.pop_back();
-        return nums;
+        return t;
     }
 };

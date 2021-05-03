67ca916ce4b1e9ca93413252d22fe93da6755d95
Bhavartha <33615252+Bhavartha@users.noreply.github.com>
2021-05-03 21:47:51 +0530

    Update Generate Parentheses.cpp

--- Generate Parentheses.cpp
+++ Generate Parentheses.cpp
@@ -2,6 +2,45 @@ class Solution {
 public:
     vector<string> v;
     
+    void re(string &s,int op,int cl){
+        if(op==0 && cl==0){
+            v.push_back(s);
+            return;
+        }
+        if(op==cl){
+            s.push_back('(');
+            re(s,op-1,cl);
+            s.pop_back();
+            return;
+        }
+        if(op==0){
+            s.push_back(')');
+            re(s,op,cl-1);
+            s.pop_back();
+            return;
+        }
+        s.push_back('(');
+        re(s,op-1,cl);
+        s.pop_back();
+        s.push_back(')');
+        re(s,op,cl-1);
+        s.pop_back();
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        string s;
+        re(s,n,n);
+        return v;
+    }
+};
+
+##########################################################
+
+
+class Solution {
+public:
+    vector<string> v;
+    
     void re(string s,int op,int cl){
         if(op==0 && cl==0){
             v.push_back(s);
